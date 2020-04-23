6932c3e8cb9769c7246ab5262f556a445a608d10
Emiliano Gutiérrez <eeggaa19@hotmail.com>
2020-04-23 16:24:47 -0500

    Correct Appoitment controller definition

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Specialty;
-use App\appointment;
+use App\Appointment;
 use App\CancelledAppointment;
 use App\Interfaces\ScheduleServiceInterface;
 
