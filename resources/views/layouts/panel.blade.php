27360dbcd95471025f225bcf2745dda3c46d8d21
Emiliano Gutiérrez <eeggaa19@hotmail.com>
2020-08-27 14:25:29 -0500

    add icon user

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -103,7 +103,7 @@
 		    <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 		      <div class="media align-items-center">
 		        <span class="avatar avatar-sm rounded-circle">
-		          <img alt="Image placeholder" src="{{ asset('/img/theme/user.JPG') }}">
+		          <img alt="Image placeholder" src="{{ asset('/img/theme/user.jpg') }}">
 		        </span>
 		        <div class="media-body ml-2 d-none d-lg-block">
 		          <span class="mb-0 text-sm  font-weight-bold">{{ auth()->user()->name }}</span>
